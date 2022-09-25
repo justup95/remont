ee90b6a5db6fc2f00df741f49e54de06d9a50d05
justup95 <justup95@gmail.com>
2022-09-26 01:48:49 +0400

    js

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,4 +1,7 @@
 // Custom Scripts
+
+
+
 // Custom scripts
 // Мобильное меню бургер
 function burgerMenu() {
@@ -42,3 +45,71 @@ function burgerMenu() {
   }
   window.addEventListener('scroll', fixedNav)
   
+
+
+
+
+
+// Модальное окно
+function bindModal(trigger, modal, close) {
+  trigger = document.querySelector(trigger),
+    modal = document.querySelector(modal),
+    close = document.querySelector(close)
+
+  const body = document.body
+
+  trigger.addEventListener('click', e => {
+    e.preventDefault()
+    modal.style.display = 'flex'
+    body.classList.add('locked')
+  });
+  close.addEventListener('click', () => {
+    modal.style.display = 'none'
+     body.classList.remove('locked')
+  });
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      modal.style.display = 'none'
+       body.classList.remove('locked')
+    }
+  })
+}
+
+// ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
+// ВТОРОЙ аргумент - класс самого модального окна.
+// ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
+bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
+
+
+
+
+
+
+//Arrow Up
+let btn = document.querySelector('.arrow-top')
+//
+function magic() {
+  if (window.pageYOffset > 1000) {
+  btn.style.opacity = '1'
+  } else { btn.style.opacity = '0' }
+}
+//
+btn.onclick = function () {
+	window.scrollTo(0,0)
+}
+
+// When scrolling, we run the function
+window.onscroll = magic
+
+
+
+
+
+// $(window).scroll(function (event) {
+//   var top = $(window).scrollTop();
+//    if(top >= 20){
+//    //show btn
+//    } else {
+//    //hide btn
+//    }
+// });
